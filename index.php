eeb872d717e759949af1fe1ff78b8eb4f3bda1e2
KARMILLA BINTI ABDUL GAIS <81630797+Karmila16@users.noreply.github.com>
2022-01-27 16:54:14 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+<body>
+  <header>
+    <h2 style="text-align: center;background-color: rgb(0, 153, 255);padding: 30px;font-size: 40px;color: white;"></h2>
+</header>
+<style>
+	body {font-family: Arial, Helvetica, sans-serif;}
+	form {border: 3px solid #f1f1f1;}
+	button {
+	  background-color: rgb(13, 164, 175);
+	  color: white;
+	  padding: 14px 30px;
+	  border: none;
+	  width: auto;
+	}
+	button:hover {
+	  opacity: 0.8;
+	}
+	</style>
+<center>
+<h2>SOC Postgraduate Subject Timetable using Population Based Metaheuristic</h2>
+<br><br>
+<button type="submit">Login</button>
+<br><br><br><br>
+</center>  
+<footer>
+  <p>
+      <h2 style="text-align: center;background-color: rgb(0, 153, 255): 30px;font-size: 12px;color: white;"></h2>
+  </p>
+</footer>
+</body>
+</html>
+</body>
+</html>
